0706207095d87eedc641227d4bf92f982005da5a
zzq <it_zhiqiang@126.com>
2021-11-15 19:25:45 +0800

    重启

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
-    "go_framework/framework"
-    "net/http"
+	"context"
+	"go_framework/framework"
+	"go_framework/framework/middleware"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-    core := framework.NewCore()
-    registerRouter(core)
-    server := &http.Server{
-        Handler: core,
-        Addr:    ":8888",
-    }
-    server.ListenAndServe()
+	core := framework.NewCore()
+
+	core.Use(middleware.Recovery())
+	core.Use(middleware.Cost())
+
+	registerRouter(core)
+	server := &http.Server{
+		Handler: core,
+		Addr:    ":8888",
+	}
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+	<-quit
+
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(timeoutCtx); err != nil {
+		log.Fatal("Server Shutdown: ", err)
+	}
 }
