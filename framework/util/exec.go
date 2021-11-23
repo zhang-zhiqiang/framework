aa0c6ac57fa45c32b7a0d4dd048b3cb6f2497696
zzq <it_zhiqiang@126.com>
2021-11-23 21:29:56 +0800

    框架目录结构设计

---
+++ framework/util/exec.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"os"
+	"syscall"
+)
+
+// GetExecDirectory 获取当前执行程序目录
+func GetExecDirectory() string {
+	file, err := os.Getwd()
+	if err == nil {
+		return file + "/"
+	}
+	return ""
+}
+
+// CheckProcessExist 判断进程是否存在，存在返回true
+func CheckProcessExist(pid int) bool {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return false
+	}
+
+	err = process.Signal(syscall.Signal(0))
+	if err != nil {
+		return false
+	}
+	return true
+}
