2abeef1f686f673b3c1ddc37cc1b5ab318c76300
zzq <it_zhiqiang@126.com>
2021-11-28 14:35:44 +0800

    配置文件读取， 定义文件目录的配置

--- app/http/module/demo/api.go
+++ app/http/module/demo/api.go
@@ -2,6 +2,7 @@ package demo
 
 import (
 	demoService "github.com/gohade/hade/app/provider/demo"
+	"github.com/gohade/hade/framework/contract"
 	"github.com/gohade/hade/framework/gin"
 )
 
@@ -32,11 +33,9 @@ func NewDemoApi() *DemoApi {
 // @Success 200 array []UserDTO
 // @Router /demo/demo [get]
 func (api *DemoApi) Demo(c *gin.Context) {
-	//appService := c.MustMake(contract.AppKey).(contract.App)
-	//baseFolder := appService.BaseFolder()
-	users := api.service.GetUsers()
-	usersDTO := UserModelsToUserDTOs(users)
-	c.JSON(200, usersDTO)
+	configService := c.MustMake(contract.ConfigKey).(contract.Config)
+	password := configService.GetString("DB_PASSWORD")
+	c.JSON(200, password)
 }
 
 // Demo godoc
--- go.mod
+++ go.mod
@@ -4,13 +4,16 @@ go 1.15
 
 require (
 	github.com/erikdubbelboer/gspt v0.0.0-20210805194459-ce36a5128377
+	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gin-contrib/sse v0.1.0
 	github.com/go-playground/validator/v10 v10.9.0
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
 	github.com/goccy/go-json v0.7.10
 	github.com/google/uuid v1.3.0
 	github.com/json-iterator/go v1.1.12
 	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/mattn/go-isatty v0.0.14
+	github.com/mitchellh/mapstructure v1.4.1
 	github.com/pkg/errors v0.9.1
 	github.com/robfig/cron/v3 v3.0.0
 	github.com/sevlyar/go-daemon v0.1.5
